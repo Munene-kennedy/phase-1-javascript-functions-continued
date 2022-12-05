1dd2df8bbc199fc2cf4f87d5babe20be0fc90547
Kennedy-munene <kennedymunene309@gmail.com>
2022-12-05 15:30:45 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+const saturdayFun = function (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  };
+  saturdayFun();
+  saturdayFun("playing online vedio games");
+
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  mondayWork();
+  mondayWork("finish work from school");
+
+  const wrapAdjective = function (wrapper = "*") {
+    const message = function (string = "special") {
+      return `You are ${wrapper}${string}${wrapper}!`;
+    };
+
+    return message;
+  };
+
+  const result = wrapAdjective("||");
+
+  const emphatic = result("great hard wprker");
+
+  console.log(emphatic);
